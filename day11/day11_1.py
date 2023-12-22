05bf8fb6c47bcf5e9b68bc3b758d774dcb26f406
Cara Lisy <lisyce@gmail.com>
2023-12-21 21:01:50 -0800

    day 11 pt 1

--- day11/day11_1.py
+++ day11/day11_1.py
@@ -18,10 +18,11 @@ def expand_universe(universe: list[list[str]]) -> list[list[str]]:
         if "#" not in row:
             result.append(row)
 
-    ptr = 0 # FIXME this part is the part that doesn't work
+    ptr = 0 
     while ptr < len(result[0]):
-        if empty_col(universe, ptr):
-            insert_empty_col(universe, ptr)
+        if empty_col(result, ptr):
+            insert_empty_col(result, ptr)
+            ptr += 1
         ptr += 1
 
     return result
@@ -37,13 +38,28 @@ def parse_universe(filename: str) -> list[list[str]]:
     return result
 
 
+def galaxy_locs(universe: list[list[str]]) -> set[tuple[int, int]]:
+    result = set()
+    for idx, row in enumerate(universe):
+        for jdx, char in enumerate(row):
+            if char == "#":
+                result.add((jdx, idx))
+    return result
+
+
+def paths_sum(universe: list[list[str]]) -> str:
+    total = 0
+    galaxies = galaxy_locs(universe)
+
+    while galaxies:
+        curr = galaxies.pop()
+        for other in galaxies:
+            total += abs(curr[0] - other[0]) + abs(curr[1] - other[1])
+
+    return total
+
 if __name__ == "__main__":
-    universe = parse_universe("input_test.txt")
+    universe = parse_universe("input.txt")
     universe = expand_universe(universe)
 
-    for row in universe:
-        for col in row:
-            print(col, end="")
-        print()
-
-    
+    print(paths_sum(universe))
